fc6417343ac6e074fd4a5287c3161d11b44fb0b7
Julia-Myjkowska-NTI-Johanneberg <julia.myjkowska@elev.ga.ntig.se>
2023-03-14 21:39:35 +0100

    3/14/23

--- js/script.js
+++ js/script.js
@@ -11,4 +11,33 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+//---------------------------//
 
+//Attempt script for background changer//
+
+const body = document.querySelector('body');
+const links = document.querySelectorAll('a');
+const imagePaths = {
+    '#1': '/img/random_image1.jpg',
+    '#2': '/img/random_image2.jpg',
+    '#3': '/img/random_image3.jpg',
+    '#4': '/img/random_image4.jpg',
+    '#5': '/img/random_image5.jpg',
+};
+
+links.forEach(link => {
+    // Add a mouseover event listener to change the background image when the mouse is over the hyperlink
+    link.addEventListener('mouseover', () => {
+      const imagePath = imagePaths[link.getAttribute('href')];
+      if (imagePath) {
+        body.style.backgroundImage = `url(${imagePath})`;
+        body.style.backgroundRepeat = 'no-repeat';
+        body.style.backgroundSize = 'cover';
+      }
+    });
+  
+    // Add a mouseout event listener to change the background image back to the original when the mouse leaves the hyperlink
+    link.addEventListener('mouseout', () => {
+      body.style.backgroundImage = 'none';
+    });
+});
